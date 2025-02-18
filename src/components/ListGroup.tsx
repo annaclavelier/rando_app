f31e3d08a213b0af1d5a48c3a885369dd43d2be5
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-18 21:31:43 +0100

    dev(tuto): ListGroup with dynamic data

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,21 @@
-
 function ListGroup() {
+  let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+  items = [];
+
+  /*const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  }*/
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
