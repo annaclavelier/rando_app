7c631beff51b68466888f2fae331ebaa018fafee
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-19 21:19:23 +0100

    dev(tuto): Add props to ListGroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   /*const getMessage = () => {
@@ -9,14 +13,20 @@ function ListGroup() {
   }*/
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
             key={item}
-            className={selectedIndex === index ? 'list-group-item active': 'list-group-item' }
-            onClick={() => {setSelectedIndex(index);}}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
