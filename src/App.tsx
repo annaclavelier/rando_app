89c65d9675b149cae14cf370f8b710a29e1eabbd
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-19 21:34:22 +0100

    dev(tuto): Alert component with children prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item:string) =>{
-    console.log(item)
-  }
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />{" "}
+      <Alert>Kepp <span>go</span>going!</Alert>
     </div>
   );
 }
