9fd3bed29a29b4531cf9fd256c186bc55d23355a
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-20 22:01:56 +0100

    datafixture: Add example datas

---
+++ src/datafixtures/rando.ts
@@ -0,0 +1,17 @@
+interface Rando{
+    titre: string;
+    difficulte: string;
+    massif: string;
+    image?: string;
+}
+
+const randos : Rando[]= [
+    {
+        titre:"L'écoutoux",
+        difficulte:"Facile",
+        massif:"Chartreuse",
+        image:""
+    }
+]
+
+export default randos;
