54626816f4003cc3ffbbb2ade0610878a1b2e742
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-20 22:38:13 +0100

    dev: SearchResults display hikes corresponding on title

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,12 +1,26 @@
 import { useSearchParams } from "react-router-dom";
+import randos from "../datafixtures/rando";
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("query");
+  const query = searchParams.get("query")?.toLowerCase() || ""; // Gestion de la casse
+
+  // Filtrer les randonnées qui contiennent le texte recherché dans le titre
+  const filteredRandos = randos.filter((rando) =>
+    rando.titre.toLowerCase().includes(query)
+  );
+
   return (
     <div className="container p-5">
       <div>
-        <h3>X résultats pour la recherche "{query}"</h3>
+        <h3>
+          {filteredRandos.length === 0 && <>Pas de résultat trouvé</>}{" "}
+          {filteredRandos.length === 1 && <>1 résultat trouvé</>}
+          {filteredRandos.length > 1 && (
+            <>{filteredRandos.length} résultats trouvés</>
+          )}{" "}
+          pour la recherche "{query}"
+        </h3>
         <hr />
       </div>
       <div id="results"></div>
