3731a138e899e1d9bc9f018c403d48555706bb84
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-20 15:26:45 +0100

    feat(header): Hero image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <div className="container-fluid">
-        <Navbar/>
+        <Navbar />
+      </div>
+      <div className="container-fluid p-0">
+        <Hero />
       </div>
     </>
   );
