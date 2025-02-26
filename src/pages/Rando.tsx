efadf58a079b43e93cede1bac60acc5fa4f9b254
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-02-26 18:55:58 +0100

    dev: Structure hike page

--- src/pages/Rando.tsx
+++ src/pages/Rando.tsx
@@ -1,8 +1,28 @@
 import { useParams } from "react-router-dom";
+import { Rando, findRando } from "../datafixtures/rando";
 
-const Rando = () => {
+const RandoPage = () => {
   const { id } = useParams();
-  return <div>Rando {id}</div>;
+
+  // Trouver l'objet correspondant
+  const rando: Rando = findRando(Number(id));
+
+  return (
+    <div className="container p-5">
+      <h1>{rando.titre}</h1>
+      <hr />
+      <div className="row">
+        <div className="col">
+          <div>
+           <h3> {rando.difficulte} - {rando.massif}</h3>
+          </div>
+          <div>Etapes : ...</div>
+        </div>
+        <div className="col">Carte</div>
+      </div>
+      <div>Galerie Photos</div>
+    </div>
+  );
 };
 
-export default Rando;
+export default RandoPage;
