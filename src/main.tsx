5142052eec5c386dd0f470c540390cef68a2d3da
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-03-06 11:02:09 +0100

    chore: Correct import + import js bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import ErrorPage from "./pages/Error.tsx";
--- src/pages/Rando.tsx
+++ src/pages/Rando.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { Rando, findRando } from "../datafixtures/rando";
+import { Rando, findRando } from "../data/rando";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
