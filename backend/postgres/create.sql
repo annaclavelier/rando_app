f9937c162564cc16375225499f5ece4ab37f9e59
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-04-10 17:16:53 +0200

    data: Sql files create/insert/drop

---
+++ backend/postgres/create.sql
@@ -0,0 +1,27 @@
+CREATE TABLE UTILISATEURS (
+    pseudo varchar(255) PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    nom varchar(100) NOT NULL ,
+    prenom varchar(100) NOT NULL
+);
+
+CREATE TABLE RANDONNEE(
+    id SERIAL PRIMARY KEY,
+    titre varchar(255),
+    description text,
+    difficulte varchar(40),
+    denivele int,
+    altitude_depart int,
+    altitude_arrivee int,
+    duree decimal,
+    km decimal,
+    massif varchar(40),
+    image varchar(255),
+    auteur varchar REFERENCES UTILISATEURS(pseudo)
+);
+
+CREATE TABLE IMAGE_SECONDAIRE(
+    chemin varchar(255) PRIMARY KEY,
+    rando_id integer  REFERENCES RANDONNEE(id)
+);
+
