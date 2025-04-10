2e5ebcc2dfbc8fe4f8de71c8b7da08e7509478b5
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-04-10 14:45:30 +0200

    dev(front): Update SearchResults with api call

--- frontend/src/pages/SearchResults.tsx
+++ frontend/src/pages/SearchResults.tsx
@@ -7,101 +7,58 @@ import axios from "axios";
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("query")?.toLowerCase() ?? ""; // Gestion de la casse
-  // Filtrer les randonnées qui contiennent le texte recherché dans le titre
+  // recherche
+  const query = searchParams.get("query")?.toLowerCase() ?? ""; 
 
-  const [allRandos, setAllRandos] = useState<Rando[]>([]);
-  const [filteredRandos, setFilteredRandos] = useState<Rando[]>([]);
+  // Résultats
+  const [randos, setRandos] = useState<Rando[]>([]);
 
-  // États pour les filtres
-  const [pendingFilters, setPendingFilters] = useState({
+  const emptyFilters = {
     difficulty: "",
     duration: "",
     massif: "",
     denivele: "",
-  });
+  };
 
-  const [activeFilters, setActiveFilters] = useState({ ...pendingFilters });
+  // Filtres provisoires => pas encore appliqués à la recherche
+  const [pendingFilters, setPendingFilters] = useState(emptyFilters);
+  // Filtres actifs => appliqués à la recherche
+  const [activeFilters, setActiveFilters] = useState(emptyFilters);
 
   const handleUpdate = () => {
+    // les filtres en attente passent en actif
     setActiveFilters({ ...pendingFilters });
-    setFilteredRandos(
-      allRandos.filter((rando) => {
-        return (
-          (!activeFilters.difficulty ||
-            rando.difficulte === activeFilters.difficulty) &&
-          (!activeFilters.duration || rando.duree === activeFilters.duration) &&
-          (!activeFilters.massif || rando.massif === activeFilters.massif) &&
-          (!activeFilters.denivele ||
-            getDeniveleCategory(rando.denivele) === activeFilters.denivele)
-        );
-      })
-    );
   };
 
-  // Réinitialiser les filtres et résultats
+  // Réinitialiser les filtres à vide
   const handleReinit = () => {
-    setPendingFilters({
-      difficulty: "",
-      duration: "",
-      massif: "",
-      denivele: "",
-    });
-    setActiveFilters({
-      difficulty: "",
-      duration: "",
-      massif: "",
-      denivele: "",
-    });
-    setFilteredRandos(allRandos);
+    setPendingFilters(emptyFilters);
+    setActiveFilters(emptyFilters);
   };
 
-  // Fonction pour catégoriser le dénivelé
-  const getDeniveleCategory = (denivele?: number) => {
-    if (denivele === undefined) return "";
-    if (denivele < 300) return "moins de 300m";
-    if (denivele < 500) return "300 à 500m";
-    if (denivele < 800) return "500 à 800m";
-    if (denivele < 1000) return "800 à 1000m";
-    return "plus 1000m";
-  };
-
-  // récupèrer les randonnées
+  // on appelle useEffect que si les filtres effectifs sont modifiés ou la query
   useEffect(() => {
-    axios.get("/api/randos")
+    //  Recherche selon filtres et query
+    axios
+      .get(
+        `/api/randos-search?query=${query}&difficulte=${activeFilters.difficulty}&duration=${activeFilters.duration}&massif=${activeFilters.massif}&denivele=${activeFilters.denivele}`
+      )
       .then((response) => {
-        setAllRandos(response.data);
+        setRandos(response.data);
       })
       .catch((error) => {
         console.error("Erreur lors du chargement des randonnées :", error);
       });
-  }, []);
-
-  // on appelle useEffect que si activeFilters est modifié ou la query !
-  useEffect(() => {
-    setFilteredRandos(
-      allRandos.filter((rando) => {
-        return (
-          rando.titre.toLowerCase().includes(query) &&
-          (!activeFilters.difficulty ||
-            rando.difficulte === activeFilters.difficulty) &&
-          (!activeFilters.duration || rando.duree === activeFilters.duration) &&
-          (!activeFilters.massif || rando.massif === activeFilters.massif) &&
-          (!activeFilters.denivele ||
-            getDeniveleCategory(rando.denivele) === activeFilters.denivele)
-        );
-      })
-    );
-  }, [query, activeFilters, allRandos]);
+  }, [query, activeFilters]);
 
   return (
     <div className="container p-5">
       <div className="d-flex justify-content-between align-items-center">
         <h3>
-          {filteredRandos.length === 0 && <>Pas de résultat trouvé</>}{" "}
-          {filteredRandos.length === 1 && <>1 résultat trouvé</>}
-          {filteredRandos.length > 1 && (
-            <>{filteredRandos.length} résultats trouvés</>
+          {randos.length === 0 && <>Pas de résultat trouvé</>}{" "}
+          {randos.length === 1 && <>1 résultat trouvé</>}
+          {randos.length > 1 && (
+            <>{randos.length} résultats trouvés</>
           )}{" "}
           pour la recherche "{query}"
         </h3>
@@ -116,7 +73,7 @@ const SearchResults = () => {
 
       <hr />
       <div id="results" className="row gy-2">
-        {filteredRandos.map((item) => (
+        {randos.map((item) => (
           <div className="col-md-6" key={item.id}>
             <CardRando
               id={item.id}
