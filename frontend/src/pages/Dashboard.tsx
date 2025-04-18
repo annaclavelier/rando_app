eea5749596b2dd2070fc9782beb576eedd7c5b05
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-04-18 22:04:41 +0200

    dev(front): Update links dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,19 +1,22 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 function Dashboard() {
   const { auth } = useAuth();
 
   return (
-    <>
       <div className="container p-5">
         <div className="h3">
           Bonjour <span className="text-capitalize">{auth?.prenom}</span> !
         </div>
-      </div>
+        <div>
+          <Link to="/my-randos">Mes randonnées</Link>
+        </div>
 
-      <div>Vos randonnées</div>
-      <div>Vos favoris</div>
-    </>
+        <div>
+          <Link to="/favorites">Mes favoris</Link>
+        </div>
+      </div>
   );
 }
 
