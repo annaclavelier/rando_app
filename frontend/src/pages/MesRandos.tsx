31d6efd5bf9138f1105cbbea587fa3c180f2cd8f
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-04-18 22:00:19 +0200

    dev(back): Add actions to randos table in MesRandos

--- frontend/src/pages/MesRandos.tsx
+++ frontend/src/pages/MesRandos.tsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 import { Rando } from "../data/rando";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEye,
+  faPenToSquare,
+  faPlus,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router";
+import axios from "axios";
 
 function MesRandos() {
   const [randos, setRandos] = useState<Rando[]>([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  const deleteRando = async (id: number) => {
+    try {
+      await axios.delete(`http://localhost:8080/api/rando/${id}`);
+    } catch (error) {
+      console.error("Erreur lors de la suppression : ", error);
+    }
+  };
 
   useEffect(() => {
     fetch("http://localhost:8080/api/mes-randos", {
@@ -45,16 +59,52 @@ function MesRandos() {
               <th>Durée</th>
               <th>Difficulté</th>
               <th>Dénivelé</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {randos.map((rando) => (
+            {randos.map((rando, index) => (
               <tr key={rando.id}>
                 <td>{rando.titre}</td>
                 <td>{rando.massif}</td>
                 <td>{rando.duree}</td>
                 <td>{rando.difficulte}</td>
                 <td>{rando.denivele} m</td>
+                <td>
+                  <div className="btn-group">
+                    <button
+                      className="btn btn-outline-primary"
+                      title="Voir"
+                      onClick={() => navigate(`/rando/${rando.id}`)}
+                    >
+                      <FontAwesomeIcon icon={faEye} />
+                    </button>
+                    <button
+                      className="btn btn-outline-secondary"
+                      title="Modifier"
+                      onClick={() => navigate(`/edit-rando/${rando.id}`)}
+                    >
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                    <button
+                      className="btn btn-outline-danger"
+                      title="Supprimer"
+                      onClick={() => {
+                        let confirmation = confirm(
+                          "Voulez vous vraiment supprimer cette randonnée ?"
+                        );
+                        if (confirmation) {
+                          // call api
+                          deleteRando(rando.id);
+                          // delete from list
+                          setRandos(randos.splice(index, 1));
+                        }
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
