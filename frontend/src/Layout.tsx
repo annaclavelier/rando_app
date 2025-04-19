fe55603d15ebc1624520dbc4bdd2ae9de203b179
Anna CLAVELIER <anna.clavelier@etu.univ-grenoble-alpes.fr>
2025-04-19 18:41:05 +0200

    dev(front): Sidebar component

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./app.css";
 import { AuthProvider } from "./context/AuthContext";
+import Sidebar from "./components/Sidebar";
 
 const Layout = () => {
   return (
       <AuthProvider>
         <Navbar />
+        <Sidebar/>
         <div className="container-fluid p-0">
           <Outlet />
         </div>
